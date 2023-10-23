ee4606a7646e0d69cae9fa25057af8f52f22386d
andrearicciotti <ricciotti.andrea1993@gmail.com>
2023-10-23 17:28:06 +0200

    add: program logic for dice-game program

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,2 +1,22 @@
+// LOGICA DEL PROGRAMMA
 
+const rolldice = document.getElementById("roll");
+rolldice.addEventListener("click", function () {
+
+    const randomNrUser = Math.floor(Math.random() * (7 - 1) + 1);
+    const randomNrCom = Math.floor(Math.random() * (7 - 1) + 1);
+    console.log(randomNrUser, randomNrCom);
+
+    if (randomNrUser === randomNrCom) {
+
+        console.log("E' un pareggio!");
+
+    } else if (randomNrUser > randomNrCom) {
+
+        console.log("Hai vinto!");
+    } else {
+
+        console.log("Hai perso!");
+    }
+})
 
